45f671329849ee1844b387e2b53076130fb0d1e0
Mario-Dimac <mario.brumini@dimacsrl.com>
2026-05-19 11:08:29 +0200

    Remove stale MainWindow helpers

--- src/gui/MainWindow.cpp
+++ src/gui/MainWindow.cpp
@@ -70,19 +70,6 @@ QString resolveProjectPath(const QString& path)
   return projectPath(path);
 }
 
-std::vector<cv::Rect> toCvRects(const QVector<QRect>& rects)
-{
-  std::vector<cv::Rect> result;
-  result.reserve(static_cast<size_t>(rects.size()));
-
-  for (const QRect& rect : rects)
-  {
-    result.emplace_back(rect.x(), rect.y(), rect.width(), rect.height());
-  }
-
-  return result;
-}
-
 bool circleFromThreePoints(const QVector<QPoint>& points, ImageCircle& circle)
 {
   if (points.size() < 3)
@@ -119,83 +106,6 @@ bool circleFromThreePoints(const QVector<QPoint>& points, ImageCircle& circle)
 
   circle = {center, radius};
   return true;
-}
-
-QString scanDirectionToRecipe(EdgeLineScanDirection direction)
-{
-  return direction == EdgeLineScanDirection::NormalNegative ? "normal_negative" : "normal_positive";
-}
-
-EdgeLineScanDirection scanDirectionFromRecipe(const QString& value)
-{
-  return value == "normal_negative" ? EdgeLineScanDirection::NormalNegative : EdgeLineScanDirection::NormalPositive;
-}
-
-QString transitionToRecipe(EdgeLineTransition transition)
-{
-  return transition == EdgeLineTransition::DarkToLight ? "dark_to_light" : "light_to_dark";
-}
-
-EdgeLineTransition transitionFromRecipe(const QString& value)
-{
-  return value == "dark_to_light" ? EdgeLineTransition::DarkToLight : EdgeLineTransition::LightToDark;
-}
-
-QString pickModeToRecipe(EdgeLinePickMode mode)
-{
-  if (mode == EdgeLinePickMode::Last)
-  {
-    return "last";
-  }
-
-  if (mode == EdgeLinePickMode::Best)
-  {
-    return "best";
-  }
-
-  return "first";
-}
-
-EdgeLinePickMode pickModeFromRecipe(const QString& value)
-{
-  if (value == "last")
-  {
-    return EdgeLinePickMode::Last;
-  }
-
-  if (value == "best")
-  {
-    return EdgeLinePickMode::Best;
-  }
-
-  return EdgeLinePickMode::First;
-}
-
-SurfaceTwoCirclesStrategyConfig toProcessorStrategy(const SurfaceLocalizationStrategyConfig& recipeConfig)
-{
-  SurfaceTwoCirclesStrategyConfig result;
-  result.originFeatureId = recipeConfig.origin.toStdString();
-  result.xAxisFromFeatureId = recipeConfig.xAxisFrom.toStdString();
-  result.xAxisToFeatureId = recipeConfig.xAxisTo.toStdString();
-
-  for (const SurfaceStrategyFeatureConfig& recipeFeature : recipeConfig.features)
-  {
-    SurfaceCircleFeatureConfig feature;
-    feature.id = recipeFeature.id.toStdString();
-    feature.polarity = recipeFeature.polarity.toStdString();
-    feature.searchRoi = cv::Rect(
-      recipeFeature.searchRoi.x(),
-      recipeFeature.searchRoi.y(),
-      recipeFeature.searchRoi.width(),
-      recipeFeature.searchRoi.height());
-    feature.threshold.minValue = recipeFeature.thresholdMin;
-    feature.threshold.maxValue = recipeFeature.thresholdMax;
-    feature.expectedRadiusMin = recipeFeature.expectedRadiusMin;
-    feature.expectedRadiusMax = recipeFeature.expectedRadiusMax;
-    result.features.push_back(feature);
-  }
-
-  return result;
 }
 
 }
